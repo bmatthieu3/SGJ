5d1bdf2a79ab38112c3c010e0af03feaf1108499
Gondror <juleswh97@gmail.com>
2023-03-11 09:26:05 +0100

    Tuto

---
+++ Assets/TutoJules/test.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    public GameObject unNom;
+    [SerializeField ]private Image nomImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nomImage = unNom.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonTest()
+    {
+        Debug.Log("Boutton fonctionne");
+    }
+}
