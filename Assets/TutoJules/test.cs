89ef709235d85c7de8557e0591374fc034180e0d
Matthieu BAUMANN <matthieubaumann@cds-dev-mb-2.local>
2023-03-11 10:45:04 +0100

    Add image assets

--- Assets/TutoJules/test.cs
+++ Assets/TutoJules/test.cs
@@ -5,24 +5,33 @@ using UnityEngine.UI;
 
 public class test : MonoBehaviour
 {
-    public GameObject unNom;
+    public GameObject gameObj;
+    [SerializeField] public Sprite m_Sprite;
+
     [SerializeField] private Image nomImage;
+
+    private float nextActionTime = 0.0f;
+    public float period = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        nomImage = unNom.GetComponent<Image>();
+        nomImage = gameObj.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       if (Time.time > nextActionTime ) {
+            nextActionTime += period;
+            // change the sprite here
+        }
     }
 
     public void ButtonTest()
     {
         Debug.Log("Boutton fonctionne");
+        nomImage.sprite = m_Sprite;
     }
 
     public void InputFieldTest()
