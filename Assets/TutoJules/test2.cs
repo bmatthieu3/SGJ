7a315bcaf5b701eddad98cf98dd91830efea52c1
Thomas Saigre <saigre@math.unistra.fr>
2023-03-11 12:32:49 +0100

    create new script for scene

--- Assets/TutoJules/test2.cs
+++ Assets/TutoJules/test2.cs
@@ -4,51 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
 
-public class test : MonoBehaviour
+public class test2 : MonoBehaviour
 {
-    [SerializeField] private Image image;
-    // Input field
-    [SerializeField] public InputField inputField;
-    // Sprites
-    [SerializeField] public Sprite[] frogSprites;
-    [SerializeField] public Sprite[] eyeSprites;
-    [SerializeField] public Sprite[] pizzaSprites;
-    [SerializeField] public Sprite[] snakeSprites;
-    private int numSpriteSets = 4;
-    private int idxSpriteSet = 0;
+    public GameObject gameObj;
+
+    [SerializeField] private Image nomImage;
+    [SerializeField] private InputField inputField;
 
     [SerializeField] private string solution = "grenouille";
 
-    private Sprite[] curSpriteSet;
-    private int idxSprite = 0;
-    // Timer
     private float nextActionTime = 0.0f;
-    public float timeBetweenTwoSpriteRes = 0.2f;
-
-    // Player score
-    private int score = 0;
+    public float period = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        inputField = GetComponent<InputField>();
-        image = GetComponent<Image>();
-
-        curSpriteSet = frogSprites;
+        nomImage = gameObj.GetComponent<Image>();
+        inputField = gameObj.GetComponent<InputField>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextActionTime) {
-            nextActionTime += timeBetweenTwoSpriteRes;
-            // change the sprite here
-            image.sprite = curSpriteSet[idxSprite % curSpriteSet.Length]; // Change the sprite of the Image object
-            idxSprite += 1;
-        }
+
     }
 
-    public void InputFieldTest()
+
+    public void ButtonTest()
+    {
+        Debug.Log("Boutton fonctionne");
+    }
+
+    public void checkSolution()
     {
         string inputText = inputField.text;
         string lower_str = inputText.ToLower();
@@ -72,4 +59,5 @@ public class test : MonoBehaviour
         }
 
     }
+
 }
