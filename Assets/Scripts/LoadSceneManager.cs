e04c99eeeb8c753291c70eec07fa5ec027261a95
Gondror <juleswh97@gmail.com>
2023-03-12 09:34:23 +0100

    A> Sprites ecran 3.2

--- Assets/Scripts/LoadSceneManager.cs
+++ Assets/Scripts/LoadSceneManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class LoadSceneManager : MonoBehaviour
 {
@@ -29,4 +30,16 @@ public class LoadSceneManager : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
